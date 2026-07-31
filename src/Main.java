dd5f9756445660ab4be9ecdcc252f470b1017a62
santiago.sosa <santiago.sosa@bemyself.com>
2026-07-31 08:03:26 -0500

    Read a txt file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String fileName = "resources/hello_unicode.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException fn) {
+            System.out.println("Could not locate file");
+        }
+        catch (IOException io) {
+            System.out.println("Something went wrong");
         }
     }
 }
